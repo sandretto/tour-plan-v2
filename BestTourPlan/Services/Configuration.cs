8930eb8106887069d3869246a539809d9cbed911
sandretto <alepet.petr1638@yandex.ru>
2020-12-20 02:22:51 +0300

    Modify services configuration

---
+++ BestTourPlan/Services/Configuration.cs
@@ -0,0 +1,12 @@
+﻿namespace BestTourPlan.Services
+{
+    public class Configuration
+    {
+        public static string ConnectionString { get; set; }
+        public static string SiteName { get; set; }
+        public static string ContactEmail { get; set; }
+        public static string Address { get; set; }
+        public static string BusinessPhone { get; set; }
+        public static string HotlinePhone { get; set; }
+    }
+}
--- BestTourPlan/Startup.cs
+++ BestTourPlan/Startup.cs
@@ -7,13 +7,51 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BestTourPlan.Services;
+using Microsoft.Extensions.Configuration;
+using BestTourPlan.Domain.Repositories.Abstract;
+using BestTourPlan.Domain.Repositories.Concrete;
+using BestTourPlan.Domain;
+using Microsoft.EntityFrameworkCore;
+using Config = BestTourPlan.Services.Configuration;
+using Microsoft.AspNetCore.Identity;
 
 namespace BestTourPlan
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; private set; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            Configuration.Bind("Project", new Config());
+
+            services.AddTransient<IHotelRepository, EFHotelRepository>();
+
+            services.AddDbContext<AppDbContext>(x => 
+                x.UseSqlServer(Config.ConnectionString));
+
+            services.AddIdentity<CustomIdentityUser, IdentityRole>(x => 
+            {
+                x.User.RequireUniqueEmail = true;
+                x.Password.RequiredLength = 5;
+            })
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(x => 
+            {
+                x.Cookie.Name = "TourPlanAuth";
+                x.Cookie.HttpOnly = true;
+                x.LoginPath = "/account/login";
+                x.AccessDeniedPath = "/account/accessdenied";
+                x.SlidingExpiration = true;
+            });
+
             services.AddControllersWithViews()
                 .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0)
                 .AddSessionStateTempDataProvider();
@@ -27,9 +65,13 @@ namespace BestTourPlan
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseRouting();
 
-            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
