83f182be099ae8f1cd3a75d554b4714c1b039ba7
sandretto <alepet.petr1638@yandex.ru>
2020-12-20 02:20:18 +0300

    Add entities

---
+++ BestTourPlan/Domain/Entities/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BestTourPlan.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        protected BaseEntity()
+        {
+            CreationDate = DateTime.UtcNow;
+        }
+
+        [Required]
+        public Guid Id { get; set; }
+        [Display(Name = "Name")]
+        public virtual string Name { get; set; }
+        [Display(Name ="Image Path")]
+        public string ImagePath { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime CreationDate { get; set; } 
+    }
+}
---
+++ BestTourPlan/Domain/Entities/Hotel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BestTourPlan.Domain.Entities
+{
+    public class Hotel : BaseEntity
+    {
+        [Required(ErrorMessage = "Please specify hotel name")]
+        [Display(Name = "Name")]
+        public override string Name { get; set; }
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Please specify hotel rating")]
+        [Display(Name = "Rating")]
+        public double Rating { get; set; }
+        [Required(ErrorMessage = "Please specify price per room")]
+        [Display(Name = "Price")]
+        public decimal Price { get; set; }
+    }
+}
