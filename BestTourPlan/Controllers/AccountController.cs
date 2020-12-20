cf7c7338251574be501c87ec722af3b7ad9bcfe8
sandretto <alepet.petr1638@yandex.ru>
2020-12-21 00:24:22 +0300

    Add Account controller and view

---
+++ BestTourPlan/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using BestTourPlan.Domain;
+using BestTourPlan.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BestTourPlan.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<CustomIdentityUser> userManager;
+        private readonly SignInManager<CustomIdentityUser> signInManager;
+        public AccountController(
+            UserManager<CustomIdentityUser> userManager, 
+            SignInManager<CustomIdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View(new LoginViewModel());
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await userManager.FindByNameAsync(model.UserName);
+                if (user != null)
+                {
+                    await signInManager.SignOutAsync();
+                    Microsoft.AspNetCore.Identity.SignInResult result = 
+                        await signInManager.PasswordSignInAsync(user, model.Password, true, false);
+                    if (result.Succeeded)
+                    {
+                        return Redirect(returnUrl ?? "/");
+                    }
+                }
+                ModelState.AddModelError(nameof(LoginViewModel.UserName), "Wrong login or password");
+            }
+            return View(model);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
