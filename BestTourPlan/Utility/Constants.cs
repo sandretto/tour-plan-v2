3584acfcfcdde5b3915412245441fb59d3607478
sandretto <alepet.petr1638@yandex.ru>
2020-12-23 01:05:03 +0300

    Add class with constants

---
+++ BestTourPlan/Utility/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BestTourPlan.Utility
+{
+    public class Constants
+    {
+        public const int MaxRating = 5;
+    }
+}
