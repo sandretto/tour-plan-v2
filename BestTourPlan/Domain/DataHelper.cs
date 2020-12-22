8cbf2c74d033095e7fc4f6f4630e633222f7e07d
sandretto <alepet.petr1638@yandex.ru>
2020-12-23 00:57:41 +0300

    Add data helper

---
+++ BestTourPlan/Domain/DataHelper.cs
@@ -0,0 +1,14 @@
+﻿using BestTourPlan.Domain.Repositories.Abstract;
+
+namespace BestTourPlan.Domain
+{
+    public class DataHelper
+    {
+        public IHotelRepository Hotels { get; set; }
+
+        public DataHelper(IHotelRepository hotelsRepository)
+        {
+            Hotels = hotelsRepository;
+        }
+    }
+}
